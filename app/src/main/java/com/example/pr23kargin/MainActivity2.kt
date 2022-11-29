8aef2b2406d90237a668e4c65036a3d8d07557b0
SashaVoin5 <sasha04ru@mail.ru>
2022-11-29 15:49:04 +0700

    iriskikiadd rabota

--- app/src/main/java/com/example/pr23kargin/MainActivity2.kt
+++ app/src/main/java/com/example/pr23kargin/MainActivity2.kt
@@ -3,6 +3,8 @@ import android.content.Intent
 import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 
 class MainActivity2 : GestureProcessing() {
@@ -18,10 +20,10 @@ class MainActivity2 : GestureProcessing() {
     override fun onSwipeRight() {
         startActivity(Intent(this, MainActivity::class.java))
     }
-    fun onClick(){
+    fun onClick(view : View){
         val db = baseContext.openOrCreateDatabase("app.db", MODE_PRIVATE, null)
         db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, UNIQUE(name))")
-        db.execSQL("INSERT OR IGNORE INTO users VALUES ('dhdh', 22), ('Пdhdhdh', 31), ('рпвпвпрв ', 28);")
+        db.execSQL("INSERT OR IGNORE INTO users VALUES ('', 22), ('', 31), (' ', 28);")
         val query: Cursor = db.rawQuery("SELECT * FROM users;", null)
         textView.text = ""
         var totalString :String = ""
@@ -30,6 +32,7 @@ class MainActivity2 : GestureProcessing() {
             val age: Int = query.getInt(1)
             totalString= totalString + "Name: $name Age: $age\n"
         }
+        Log.e("zhopa", totalString)
         textView.text = totalString
 
         query.close()
